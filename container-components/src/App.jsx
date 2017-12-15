a490e40f34f9706785ddeb7d05c61b8afe24ac3c
ThiagoRAD <thauroo@gmail.com>
2017-12-14 22:51:20 -0300

    data loader

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,20 +1,31 @@
+import axios from 'axios'
 import {CurrentUserLoader} from './CurrentUserLoader';
+import DataLoader from './DataLoader'
 import ResourceLoader from './ResourceLoader'
 import {UserInfo} from './UserInfo';
 import UserLoader from './UserLoader';
 
+const getServerData = url => async () => {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+
 function App() {
   return (
     <>
-      {/* <CurrentUserLoader>
+      <CurrentUserLoader>
         <UserInfo />
-      </CurrentUserLoader> */}
-      {/* <UserLoader userId={1}>
+      </CurrentUserLoader>
+      <UserLoader userId={1}>
         <UserInfo />
-      </UserLoader> */}
+      </UserLoader>
       <ResourceLoader resourceUrl="/api/users/1" resourceName="user">
         <UserInfo />
       </ResourceLoader>
+      <DataLoader resourceName="user" getData={getServerData('/api/users/1')}>
+        <UserInfo />
+      </DataLoader>
     </>
   );
 }
