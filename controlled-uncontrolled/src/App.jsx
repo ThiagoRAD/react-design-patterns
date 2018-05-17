ba12944c7f1b5cdfe92607877c449f567aabb1d1
ThiagoRAD <thauroo@gmail.com>
2018-05-17 18:51:15 -0300

    reformat controlled and uncontrolled onboarding

--- controlled-uncontrolled/src/App.jsx
+++ controlled-uncontrolled/src/App.jsx
@@ -1,21 +1,61 @@
-import { useState } from 'react'
-import './App.css'
-import {ControlledForm} from './ControlledForm';
-import { ControlledModal } from './ControlledModal'
+import {useState} from 'react';
+import {ControlledOnboardingFlow} from './ControlledOnboardingFlow';
+import {UncontrolledOnboardingFlow} from './UncontrolledOnboardingFlow';
 
-const App = () => {
+const StepOne = ({goToNext}) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({name: 'John Doe'})}>Next</button>
+  </>
+);
 
-  const [showModal, setShowModal] = useState(false);
+const StepTwo = ({goToNext}) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({age: 40})}>Next</button>
+  </>
+);
+
+const StepThree = ({goToNext}) => (
+  <>
+    <h1>Congratulations! You qualify for the senior discount!</h1>
+    <button onClick={() => goToNext({hairColor: 'brown'})}>Next</button>
+  </>
+);
+
+const StepFour = ({goToNext}) => (
+  <>
+    <h1>Step 4</h1>
+    <button onClick={() => goToNext({hairColor: 'brown'})}>Next</button>
+  </>
+);
+
+function App() {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = (stepData) => {
+    setOnboardingData({...onboardingData, ...stepData});
+    setCurrentIndex(currentIndex + 1);
+  };
 
   return (
-    <div>
-      <button onClick={() => setShowModal(true)}>Show Modal</button>
-      <ControlledModal shouldShow={showModal} onRequestClose={() => setShowModal(false)}>
-        <ControlledForm />
-      </ControlledModal>
+    <>
+      <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext} onFinish={() => alert('Done!')}>
+        <StepOne />
+        <StepTwo />
+        {onboardingData.age >= 65 && <StepThree />}
+        <StepFour />
+      </ControlledOnboardingFlow>
       <hr />
-    </div>
+      <UncontrolledOnboardingFlow>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+        <StepFour />
+      </UncontrolledOnboardingFlow>
+    </>
   );
-};
+}
 
 export default App;
