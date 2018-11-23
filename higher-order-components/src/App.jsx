aa4ddcbf0d8056899ce9bf5f220592df90d805e1
ThiagoRAD <thauroo@gmail.com>
2018-11-23 19:13:45 -0300

    wrapping a component with higher order components

--- higher-order-components/src/App.jsx
+++ higher-order-components/src/App.jsx
@@ -2,7 +2,9 @@ import {printProps} from './printProps';
 import {UserInfo} from './UserInfo';
 
 function App() {
-  const EnhancedComponent = printProps(UserInfo);
+  const UserInfoWrapped = printProps(UserInfo);
+
+  return <UserInfoWrapped a={1} b="Hello" c={{name: 'Black'}} user={{id: 1, name: 'John Doe', hobbies: ['Reading', 'Traveling']}}></UserInfoWrapped>
 }
 
 export default App;
