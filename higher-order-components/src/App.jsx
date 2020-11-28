5638098aeeeccadd5fb433ca886b28213ca98710
ThiagoRAD <thauroo@gmail.com>
2020-11-28 19:14:52 -0300

    displaying props to the console with utilitary higher order component

--- higher-order-components/src/App.jsx
+++ higher-order-components/src/App.jsx
@@ -4,7 +4,7 @@ import {UserInfo} from './UserInfo';
 function App() {
   const UserInfoWrapped = printProps(UserInfo);
 
-  return <UserInfoWrapped a={1} b="Hello" c={{name: 'Black'}} user={{id: 1, name: 'John Doe', hobbies: ['Reading', 'Traveling']}}></UserInfoWrapped>
+  return <UserInfoWrapped user={{id: 1, name: 'Black', hobbies: ['Reading', 'Traveling']}}></UserInfoWrapped>
 }
 
 export default App;
