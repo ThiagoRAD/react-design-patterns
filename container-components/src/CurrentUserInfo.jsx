68a9fdcb08d9a5b155295b77c3f845a43564fdc0
ThiagoRAD <thauroo@gmail.com>
2021-08-31 13:29:48 -0300

    currentuserinfo

--- container-components/src/CurrentUserInfo.jsx
+++ container-components/src/CurrentUserInfo.jsx
@@ -1,3 +1,16 @@
-export const CurrentUserInfo = () => {
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
+export const CurrentUserInfo = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get('/api/current-user')
+      setUser(response.data)
+    })
+  }, [])
+
+  return <></>
 }
