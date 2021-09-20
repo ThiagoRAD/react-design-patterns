56335253bcc41efbbd54e8f074fbc6ede85137aa
ThiagoRAD <thauroo@gmail.com>
2021-09-20 18:51:15 -0300

    reformat controlled and uncontrolled onboarding

--- controlled-uncontrolled/src/ControlledOnboardingFlow.jsx
+++ controlled-uncontrolled/src/ControlledOnboardingFlow.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
-export const ControlledOnboardingFlow = ({ children, onFinish, currentIndex, onNext }) => {
-	const goToNext = stepData => {
-		onNext(stepData);
-	}
+export const ControlledOnboardingFlow = ({children, onFinish, currentIndex, onNext}) => {
+  const goToNext = (stepData) => {
+    onNext(stepData);
+  };
 
-	if (currentIndex >= React.Children.toArray(children).length) {
-		onFinish();
-		return <h1>Done!</h1>
-	}
+  if (currentIndex >= React.Children.toArray(children).length) {
+    onFinish();
+    return <h1>Done!</h1>;
+  }
 
-	const currentChild = React.Children.toArray(children)[currentIndex];
+  const currentChild = React.Children.toArray(children)[currentIndex];
 
-	if (React.isValidElement(currentChild)) {
-		return React.cloneElement(currentChild, { goToNext });
-	}
-
-	return currentChild;
-}
+  return React.cloneElement(currentChild, {goToNext});
+};
