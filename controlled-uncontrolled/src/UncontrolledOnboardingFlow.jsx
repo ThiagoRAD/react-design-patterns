fbc95039cd2aecf4769c1c808a0049d1073f4695
ThiagoRAD <thauroo@gmail.com>
2021-11-19 18:51:15 -0300

    reformat controlled and uncontrolled onboarding

--- controlled-uncontrolled/src/UncontrolledOnboardingFlow.jsx
+++ controlled-uncontrolled/src/UncontrolledOnboardingFlow.jsx
@@ -1,33 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
-	const [onboardingData, setOnboardingData] = useState({});
-	const [currentIndex, setCurrentIndex] = useState(0);
+export const UncontrolledOnboardingFlow = ({children, onFinish}) => {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	const goToNext = stepData => {
-		const nextIndex = currentIndex + 1;
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1;
 
-		const updatedData = {
-			...onboardingData,
-			...stepData,
-		};
+    const updatedData = {
+      ...onboardingData,
+      ...stepData,
+    };
 
-		console.log(updatedData);
+    console.log(updatedData);
 
-		if (nextIndex < children.length) {
-			setCurrentIndex(nextIndex);
-		} else {
-			onFinish(updatedData);
-		}
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
 
-		setOnboardingData(updatedData);
-	}
+    setOnboardingData(updatedData);
+  };
 
-	const currentChild = React.Children.toArray(children)[currentIndex];
+  const currentChild = React.Children.toArray(children)[currentIndex];
 
-	if (React.isValidElement(currentChild)) {
-		return React.cloneElement(currentChild, { goToNext });
-	}
-
-	return currentChild;
-}
+  return React.cloneElement(currentChild, {goToNext});
+};
