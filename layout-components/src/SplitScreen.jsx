949bc31f49efa5b436bf8c929d892442150db719
ThiagoRAD <thauroo@gmail.com>
2022-10-30 22:26:14 -0300

    splitscreen

--- layout-components/src/SplitScreen.jsx
+++ layout-components/src/SplitScreen.jsx
@@ -1,5 +1,30 @@
-const SplitScreen = () => {
-  return <div></div>
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Pane = styled.div`
+  flex: 1;
+`;
+
+const SplitScreen = ({ left: Left, right: Right}) => {
+  return (
+  <Container>
+    <Pane>
+      <Left />
+    </Pane>
+    <Pane>
+      <Right />
+    </Pane>
+  </Container>
+  )
 }
+
+SplitScreen.propTypes = {
+  left: PropTypes.elementType.isRequired,
+  right: PropTypes.elementType.isRequired,
+};
 
 export default SplitScreen;
