d498048906fc5a1ab24b76f7133c8ed9183a373f
ThiagoRAD <thauroo@gmail.com>
2023-05-14 21:53:32 -0300

    userloader

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,12 +1,18 @@
-import { CurrentUserLoader } from './CurrentUserLoader'
-import { UserInfo } from './UserInfo'
+import {CurrentUserLoader} from './CurrentUserLoader';
+import {UserInfo} from './UserInfo';
+import UserLoader from './UserLoader';
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
-  )
+    <>
+      {/* <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader> */}
+      <UserLoader userId={1}>
+        <UserInfo />
+      </UserLoader>
+    </>
+  );
 }
 
-export default App
+export default App;
