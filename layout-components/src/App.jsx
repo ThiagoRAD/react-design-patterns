da63ec837bafc07d353c89aff83cc9003a2f7575
ThiagoRAD <thauroo@gmail.com>
2023-12-21 22:27:23 -0300

    different colors

--- layout-components/src/App.jsx
+++ layout-components/src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css'
 import SplitScreen from './SplitScreen'
+import styled from 'styled-components';
+
+const LeftContainer = styled.div`
+  background-color: lightblue;
+  height: 100vh;
+`;
+
+const RightContainer = styled.div`
+  background-color: lightgreen;
+  height: 100vh;
+`;
+
+const LeftHandComponent = () => (
+  <LeftContainer>
+    <h1>Left Side</h1>
+  </LeftContainer>
+);
+
+const RightHandComponent = () => (
+  <RightContainer>
+    <h1>Right Side</h1>
+  </RightContainer>
+);
+
 
 function App() {
 
   return (
     <>
-      <SplitScreen left={() => <h1>Left Side</h1>} right={() => <h1>Right Side</h1>} />
+      <SplitScreen left={LeftHandComponent} right={RightHandComponent} />
     </>
   )
 }
