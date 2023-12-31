d8b41e4a7481b4ca8b6c7e7e3ff4ada19cd23f0f
ThiagoRAD <thauroo@gmail.com>
2023-12-30 21:43:17 -0300

    small person list item

--- layout-components/src/SmallPersonListItem.jsx
+++ layout-components/src/SmallPersonListItem.jsx
@@ -1,5 +1,8 @@
-const SmallPersonListItem = () => {
-
+const SmallPersonListItem = ({person}) => {
+  const {name, age} = person;
+  return (
+    <p>Name: {name}, Age: {age} years</p>
+  )
 }
 
 export default SmallPersonListItem;
