eb6e307af69c187601c89970d67710f1fe9233dc
ThiagoRAD <thauroo@gmail.com>
2024-10-19 21:39:45 -0300

    regular list

--- layout-components/src/RegularList.jsx
+++ layout-components/src/RegularList.jsx
@@ -1,5 +1,13 @@
-const RegularList = () => {
-
+const RegularList = ({items, resourceName, itemComponent: ItemComponent}) => {
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>
+          <ItemComponent {...{[resourceName]: item}} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default RegularList;
