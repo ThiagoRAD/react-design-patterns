cca9eb3d98dce701f8168f896c5dc6a3324afaed
ThiagoRAD <thauroo@gmail.com>
2025-01-16 21:53:32 -0300

    userloader

--- container-components/src/UserLoader.jsx
+++ container-components/src/UserLoader.jsx
@@ -1,5 +1,24 @@
-const UserLoader = ({ id }) => {
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+const UserLoader = ({ children, userId }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(`/api/users/${userId}`)
+      console.log(response.data)
+      console.log(response)
+      setUser(response.data)
+    })()
+  }, [userId])
+
+  return user && React.Children.map(children, child => {
+    if(React.isValidElement(child)) {
+      return React.cloneElement(child, { user })
+    }
+    return child
+  })
 }
 
 export default UserLoader
