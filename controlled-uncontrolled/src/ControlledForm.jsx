5601435041725092356c0457ac4c9d8aa6df082d
ThiagoRAD <thauroo@gmail.com>
2025-09-06 17:21:08 -0300

    error message

--- controlled-uncontrolled/src/ControlledForm.jsx
+++ controlled-uncontrolled/src/ControlledForm.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const ControlledForm = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
   const [hairColor, setHairColor] = useState('#000000');
+  const [nameError, setNameError] = useState('');
+
+  useEffect(() => {
+    name.length < 2 ? setNameError('Name must be at least 2 characters long') : setNameError('');
+  }, [name]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -15,6 +20,7 @@ export const ControlledForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {nameError && <p style={{ color: 'red' }}>{nameError}</p>}
       <input name="name" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
       <input name="age" type="number" placeholder="Age" value={age} onChange={(e) => setAge(e.target.value)} />
       <input name="hairColor" type="color" placeholder="Hair Color" value={hairColor} onChange={(e) => setHairColor(e.target.value)} />
